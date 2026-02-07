6b3a85f425de72922045347557849ea21f8d544c
Denis Hrytsin <pleyden8@gmail.com>
2026-02-08 01:01:16 +0200

    Fix swiper

--- src/js/feedbacks/feedbacks-index.js
+++ src/js/feedbacks/feedbacks-index.js
@@ -1,6 +1,8 @@
 import { getDatoFromApi } from '../axios/get-api';
 import { renderFeedbacks } from './feedbacks-render';
+import { initSwiper } from './feedbacks-swiper';
 
 const response = await getDatoFromApi('/feedbacks', 'limit=3&page=1');
 console.log(response.data);
 renderFeedbacks(response.data.data);
+initSwiper();
--- src/js/feedbacks/feedbacks-swiper.js
+++ src/js/feedbacks/feedbacks-swiper.js
@@ -5,14 +5,16 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const swiper = new Swiper('.swiper', {
-  direction: 'vertical',
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: 'swiper-button-next',
-    prevEl: 'swiper-button-prev',
-  },
-});
+export function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    direction: 'horizontal',
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}

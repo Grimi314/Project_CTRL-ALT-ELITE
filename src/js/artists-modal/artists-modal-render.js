028470d002607000f09dc718549b0c9f4bf6c22d
luuushpak <lushpak0111@gmail.com>
2026-02-09 00:30:05 +0200

    create new markup

--- src/js/artists-modal/artists-modal-render.js
+++ src/js/artists-modal/artists-modal-render.js
@@ -40,7 +40,7 @@ export async function markup() {
     intDiedYear,
   } = infoAboutArtist;
 
-  //   if (!intDiedYear) intDiedYear = 'present';
+  const valueDiedYear = intDiedYear ?? 'present';
 
   const artistGenres = genres
     .map(el => {
@@ -49,50 +49,45 @@ export async function markup() {
     .join('');
 
   return `
-    <h2 class="section-modal-title">${strArtist}</h2>
-      <div class="wrapper-desctope-view">
-        <img
-          data-section-modal-img
-          src="${strArtistThumb}"
-          alt="The photo of artist"
-          class="section-modal-img"
-        />
-        <div class="wrapper-desctope-view-description">
-          <div class="section-modal-wrapper-text">
-            <div class="section-modal-wrapper-text-description">
-              <div class="section-modal-text-container">
-                <h3 class="section-modal-text-container-title">Years active</h3>
-                <p data-years class="section-modal-text-container-text">
-                  ${intFormedYear}-${intDiedYear}
-                </p>
-              </div>
+<h2 class="section-modal-title">${strArtist}</h2>
+<div class="wrapper-desctope-view">
+  <img
+    data-section-modal-img
+    src="${strArtistThumb}"
+    alt="artist image"
+    class="section-modal-img"
+  />
+  <div class="wrapper-desctope-view-description">
+    <div class="section-modal-wrapper-text">
+      <div class="section-modal-wrapper-text-description">
+        <div class="section-modal-text-container">
+          <h3 class="section-modal-text-container-title">Years active</h3>
+          <p data-years class="section-modal-text-container-text">
+            ${intFormedYear}-${valueDiedYear}
+          </p>
+        </div>
 
-              <div class="section-modal-text-container">
-                <h3 class="section-modal-text-container-title">Sex</h3>
-                <p data-sex class="section-modal-text-container-text">${strGender}</p>
-              </div>
+        <div class="section-modal-text-container">
+          <h3 class="section-modal-text-container-title">Sex</h3>
+          <p data-sex class="section-modal-text-container-text">${strGender}</p>
+        </div>
 
-              <div class="section-modal-text-container">
-                <h3 class="section-modal-text-container-title">Members</h3>
-                <p data-members class="section-modal-text-container-text">${intMembers}</p>
-              </div>
+        <div class="section-modal-text-container">
+          <h3 class="section-modal-text-container-title">Members</h3>
+          <p data-members class="section-modal-text-container-text">${intMembers}</p>
+        </div>
 
-              <div class="section-modal-text-container">
-                <h3 class="section-modal-text-container-title">Country</h3>
-                <p data-country class="section-modal-text-container-text">
-                  ${strCountry}
-                </p>
-              </div>
-            </div>
-            <div class="section-modal-text-container">
-              <h3 class="section-modal-text-container-title">Biography</h3>
-              <p class="">
-                ${strBiographyEN}
-              </p>
-            </div>
-          </div>
-          <div class="section-modal-wrapper-genres">${artistGenres}<div/>
+        <div class="section-modal-text-container">
+          <h3 class="section-modal-text-container-title">Country</h3>
+          <p data-country class="section-modal-text-container-text">${strCountry}</p>
         </div>
       </div>
+      <div class="section-modal-text-container">
+        <h3 class="section-modal-text-container-title">Biography</h3>
+        <p class="">${strBiographyEN}</p>
+      </div>
+    </div>
+  </div>
+</div>
 `;
 }

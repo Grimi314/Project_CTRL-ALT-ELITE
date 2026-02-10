1b9176d2ec542bc73551a9f246411bb2ea291e8f
AnastasiiaNotych <notychwork@gmail.com>
2026-02-10 23:54:04 +0100

    change imports in main.js file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-import './js/artists/artists-index.js';
-import './js/artists-modal/artists-modal-index.js';
-import './js/feedbacks/feedbacks-index.js';
 import './js/other-section/header.js';
-import './js/other-section/about-us.js';
-import './js/other-section/footer.js';
-import './js/other-section/submit-feedback.js';
+import './js/artists/artists-index.js';
+
+let heavyLoaded = false;
+
+window.addEventListener('load', () => {
+  import('./js/other-section/about-us.js');
+  import('./js/other-section/footer.js');
+});
+
+function loadHeavyOnce() {
+  if (heavyLoaded) return;
+  heavyLoaded = true;
+
+  import('./js/artists-modal/artists-modal-index.js');
+  import('./js/feedbacks/feedbacks-index.js');
+  import('./js/other-section/submit-feedback.js');
+
+  window.removeEventListener('pointerdown', loadHeavyOnce);
+  window.removeEventListener('keydown', loadHeavyOnce);
+}
+
+window.addEventListener('pointerdown', loadHeavyOnce, { once: true });
+window.addEventListener('keydown', loadHeavyOnce, { once: true });
